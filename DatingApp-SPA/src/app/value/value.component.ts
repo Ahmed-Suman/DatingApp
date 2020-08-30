e2d7acd2c0a4529b79f682733b4c6774d9d85cbc
Suman <suman120000@gmail.com>
2020-08-30 14:22:48 +0600

    End of Registration

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -19,7 +19,7 @@ export class ValueComponent implements OnInit {
 
 
   getValues(){
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+    this.http.get('http://localhost:14355/api/values').subscribe(response => {
       this.values = response;
     }, error =>
     {
